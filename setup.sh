10a4d13c58df0c4436ae00b085ce61127210dcc7
ProNodeRunner <pronoderunner@gmail.com>
2025-04-11 16:35:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ show_menu() {
     echo -e "${ORANGE}"
     curl -sSf "$LOGO_URL" 2>/dev/null || echo -e "=== Server Management ==="
     echo -e "\n\n\n"
-    echo " ༺ Управление сервером по кайфу v4.2 ༻ "
+    echo " ༺ Управление сервером по кайфу v4.3 ༻ "
     echo "======================================="
     echo "1) Установить новый сервер"
     echo "2) Проверить загрузку ресурсов"
